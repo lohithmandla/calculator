7534655779be1ba8365b6e74e80a63955e6789f9
lohithmandla <125439040+lohithmandla@users.noreply.github.com>
2023-05-31 16:09:36 +0530

    Update Addition.java

--- src/com/lohith/calculator/Addition.java
+++ src/com/lohith/calculator/Addition.java
@@ -6,7 +6,7 @@ public class Addition {
 		int num1=10;
 		int num2=23;
 		int result=num1*num2;
-		System.out.println("lohith is a bad boy "+result);
+		System.out.println("lohith is a good boy "+result);
 	}
 
 }
