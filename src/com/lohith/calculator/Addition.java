05042fc6eb3ae93e6746dcb50abe4dc1667570ea
lohithmandla <lohith09876@gmail.com>
2023-05-31 14:55:38 +0530

    addtion

---
+++ src/com/lohith/calculator/Addition.java
@@ -0,0 +1,12 @@
+package com.lohith.calculator;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int num1=10;
+		int num2=23;
+		int result=num1+num2;
+		System.out.println("additon of numbers is "+result);
+	}
+
+}
